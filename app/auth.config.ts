62c13cf0b60eaa73427eaeb0fc88cbece1e1cc72
visionliao <993015089@qq.com>
2025-07-14 14:37:36 +0800

    删除无用代码

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -9,19 +9,6 @@ export const authConfig = {
     // while this file is also used in non-Node.js environments
   ],
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      let isLoggedIn = !!auth?.user;
-      let isOnDashboard = nextUrl.pathname.startsWith('/protected');
-
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/protected', nextUrl));
-      }
-
-      return true;
-    },
     async jwt({ token, user }) {
       if (user) {
         token.nickname = (user as any).nickname;
